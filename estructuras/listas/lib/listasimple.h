357708c2be884a5ae3ade6443cb338cab3f6aea4
Rodrigo Nunez <vsurak@gmail.com>
2021-09-10 08:59:39 -0600

    agregando funciones a lista simple

--- estructuras/listas/lib/listasimple.h
+++ estructuras/listas/lib/listasimple.h
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include "nodo.h"
 
+#define POSITION_BEGINNING 0
+#define POSITION_END 99999999
 
 struct listasimple {
     int size = 0;
@@ -18,6 +20,7 @@ struct listasimple {
         return size;
     }
 
+/*
     void add(int pValue) {
         nodo* newvalue = (nodo*)malloc(sizeof(struct nodo));
         newvalue->data = pValue;
@@ -28,11 +31,77 @@ struct listasimple {
         } else {
             // agregar al final de la lista
             end->next = newvalue; // esto es equivalente a  (*end).next = &newvalue;            
-            end = newvalue;            
+            end = newvalue;      
         }
 
         size++;
     }
+
+    void addAtBeginning(int pValue) {
+        nodo* newvalue = (nodo*)malloc(sizeof(struct nodo));
+        newvalue->data = pValue;
+        
+        if (size == 0) {
+            start = newvalue;
+            end = newvalue;  // end = start;
+        } else {
+            newvalue->next = start;
+            start = newvalue;
+        }
+
+        size++;
+    }
+
+*/
+
+    void add(int pValue) {
+        insert(pValue, POSITION_END);
+    }
+
+    void addAtBeginning(int pValue) {
+        insert(pValue, POSITION_BEGINNING);
+    }
+
+    int insert(int pValue, int pPosition) {
+        nodo* newvalue = (nodo*)malloc(sizeof(struct nodo));
+        newvalue->data = pValue;
+        int result = 0;
+
+        if (size==0) {
+            start = newvalue;
+            end = newvalue;  
+        } else if (pPosition==0) {
+            newvalue->next = start;
+            start = newvalue;
+        } else {
+            int actualPosition = 1;
+            nodo* pointerToPosition = nullptr;
+            nodo* pointerBehind = nullptr;
+
+            if (pPosition>=size) {
+                pointerBehind = end;
+                end = newvalue;
+                actualPosition = size;
+            } else {
+                pointerToPosition = start->next;
+                pointerBehind = start;
+            }
+
+            while (actualPosition<pPosition && pointerToPosition!=nullptr) {
+                pointerBehind = pointerToPosition;
+                pointerToPosition = pointerToPosition->next;
+                actualPosition++;
+            }
+
+            pointerBehind->next = newvalue;
+            newvalue->next = pointerToPosition;
+            result = actualPosition;
+        }
+
+        size++;
+        return result;
+    }
+
 };
 
 
--- estructuras/listas/listasprog.cpp
+++ estructuras/listas/listasprog.cpp
@@ -7,16 +7,34 @@ using namespace std;
 int main() {
     listasimple milista;
 
+
     milista.add(23);
     milista.add(10);
     milista.add(2);
 
-    cout << milista.isEmpty() << " " << milista.getSize() << endl;
+    milista.addAtBeginning(100);
+    milista.addAtBeginning(120);
+
+    milista.insert(20,0);
+    milista.insert(10,0);
+    milista.insert(5,0);
+
+    int posicioninsertada = milista.insert(65, 10);
+    cout << posicioninsertada << endl;
+
+    posicioninsertada = milista.insert(15, 4);
+    cout << posicioninsertada << endl;
+
+    posicioninsertada = milista.insert(17, 9);
+    cout << posicioninsertada << endl;
+
 
     nodo* recorrido = milista.start;
     while (recorrido!=nullptr) {
         cout << recorrido->data << endl;
         recorrido = recorrido->next;
     }
+
+    cout << milista.isEmpty() << " " << milista.getSize() << endl;
 
 }
