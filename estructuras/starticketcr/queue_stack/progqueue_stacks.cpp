b1dffd4977149b4e4e5273736bc78981a83c3846
Rodrigo Nunez <vsurak@gmail.com>
2022-09-02 08:48:00 -0600

    primer version de colas chafa

---
+++ estructuras/starticketcr/queue_stack/progqueue_stacks.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "queue.h"
+
+using namespace std;
+
+int main() {
+    Queue<string>* colaNombres = new Queue<string>();
+    Queue<int>* numbersqueue = new Queue<int>();
+
+    colaNombres->enqueue(new string("el primer mensaje que meto"));
+    colaNombres->enqueue(new string("el segundo mensaje que meto"));
+    colaNombres->enqueue(new string("el tercero mensaje que meto"));
+
+    int valores[] = {30, 76, 88, 34, 12, 24};
+    
+    numbersqueue->enqueue(&valores[0]);
+    numbersqueue->enqueue(&valores[1]);
+    numbersqueue->enqueue(&valores[2]);
+    numbersqueue->enqueue(&valores[3]);
+    numbersqueue->enqueue(&valores[4]);
+    numbersqueue->enqueue(&valores[5]);
+
+
+    while (!colaNombres->isEmpty()) {
+        cout << *colaNombres->dequeue() << endl;
+    }
+
+    while (!numbersqueue->isEmpty()) {
+        cout << *numbersqueue->dequeue() << endl;
+    }
+
+}
---
+++ estructuras/starticketcr/queue_stack/queue.h
@@ -0,0 +1,45 @@
+#include "../generic/List.h"
+
+#ifndef QUEUE
+
+#define QUEUE 1
+
+template <class T>
+class Queue {
+    private:
+        List<T>* queueList;
+    
+
+    public:
+        Queue() {
+            queueList = new List<T>();
+        }
+
+        // agregar al final
+        void enqueue(T* pValue) {
+            queueList->add(pValue);
+        }
+
+        T* dequeue() {
+            T* result = NULL;
+            if (!queueList->isEmpty()) {
+                result = queueList->find(0);
+                queueList->remove(0);               
+            }
+            return result;            
+        }
+
+        T* front() {
+            T* result = NULL;
+            if (!queueList->isEmpty()) {
+                result = queueList->find(0);
+            }
+            return result;
+        }
+
+        bool isEmpty() {
+            return queueList->isEmpty();
+        }
+};
+
+#endif
