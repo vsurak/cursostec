1ce0ae283cc8cbce68eae68ea7e5874a5a24615f
rodrigo nunez <vsurak@gmail.com>
2023-05-12 11:30:07 -0600

    mapr example part I

--- bases II/MiPrimerMapR/src/mapers/SalesMaper.java
+++ bases II/MiPrimerMapR/src/mapers/SalesMaper.java
@@ -1,38 +1,25 @@
 package mapers;
 import java.io.IOException;
 
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;   
-import org.apache.hadoop.io.MapWritable;   
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.FloatWritable;   
 import org.apache.hadoop.io.Text;    
 import org.apache.hadoop.mapred.MapReduceBase;    
 import org.apache.hadoop.mapred.Mapper;    
 import org.apache.hadoop.mapred.OutputCollector;    
 import org.apache.hadoop.mapred.Reporter;  
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
-public class SalesMaper extends MapReduceBase implements Mapper<LongWritable,Text,Text,MapWritable> { 
+public class SalesMaper extends MapReduceBase implements Mapper<LongWritable,Text,Text,FloatWritable> { 
     
-    public void map(LongWritable key, Text value, OutputCollector<Text,MapWritable> output, Reporter reporter) throws IOException{    
-        JSONParser parser = new JSONParser();
-        JSONObject json =  null;
-        MapWritable timecoinsmap = new MapWritable();
-        
-        try {
-            json = (JSONObject)parser.parse(value.toString());        	
-        } catch (ParseException exp) {
-        	exp.printStackTrace();
-        }
-        
-        String year = ((String)json.get("posttime")).split("-")[0];        
-        Text metayear = new Text((String)json.get("metaverso")+","+year);
-        
-        timecoinsmap.put(new Text("coins"), new IntWritable(Integer.parseInt(json.get("coinsspent").toString())));
-        timecoinsmap.put(new Text("minutes"), new IntWritable(Integer.parseInt(json.get("timespent").toString())));
-        
-        
-        output.collect(metayear, timecoinsmap); 
+    public void map(LongWritable key, Text lineFromFile, OutputCollector<Text,FloatWritable> output, Reporter reporter) throws IOException{    
+    	String year;
+    	float amount;
+    	
+    	String values[] = lineFromFile.toString().split(",");
+    	amount = Float.parseFloat(values[1]); // paso el monto a float
+    	
+    	year = values[0].split("/")[2];
+    	        
+        output.collect(new Text(year), new FloatWritable(amount)); 
     }
 }
