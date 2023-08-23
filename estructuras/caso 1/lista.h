809bff42f38dd7aa59612d691cea67537bf3b32b
rodrigo nunez <vsurak@gmail.com>
2023-08-23 09:30:25 -0600

    ejercio week 5

---
+++ estructuras/caso 1/lista.h
@@ -0,0 +1,9 @@
+// los metodos van a ser en funcion de las operaciones TAD de una lista
+
+void insertar(Node);
+
+void delete(Node);
+
+Node *listar();
+
+Node *buscar(string palabraDeBusqueda);
---
+++ estructuras/caso 1/titulares.h
@@ -0,0 +1,9 @@
+// los metodos van a ser en funcion de las operaciones TAD de una lista
+
+void insertar(Titular);
+
+void delete(Titular);
+
+Titular *listar();
+
+Titular *buscar(string palabraDeBusqueda);
