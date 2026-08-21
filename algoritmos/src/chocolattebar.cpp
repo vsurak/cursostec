d736f0f15106343a5584468a0ce8166deb9ef413
rodrigo nunez <vsurak@gmail.com>
2026-08-21 10:52:05 -0600

    circulos concentricos

--- algoritmos/src/chocolattebar.cpp
+++ algoritmos/src/chocolattebar.cpp
@@ -15,11 +15,18 @@ public:
 
     // retornar la cantidad de cortes, debe encontrar la cantidad minima que se debe hacer
     int cuts() {
+
         auto start = std::chrono::high_resolution_clock::now();
         int totalCuts = 0;
 
 
         // put your algorithm here
+        // put your prompt here
+        // Inputs: parámetros del algoritmo, tipo de dato y el nombre
+        // Steps: 
+        // 1. saque los puntos x, y bla bla bla
+        // 2. otra cosa
+        // 3. Returne lista de circulos concentricos 
 
 
         auto end = std::chrono::high_resolution_clock::now();
@@ -42,12 +49,12 @@ private:
 int main() {
     std::srand(static_cast<unsigned int>(std::time(nullptr)));
 
-    int costosAncho[5];
+    int costosAncho[500];
     for (int &costo : costosAncho) {
         costo = 2 + std::rand() % 5; // valores de 2 a 6
     }
 
-    int costosAlto[3];
+    int costosAlto[300];
     for (int &costo : costosAlto) {
         costo = 7 + std::rand() % 5; // valores de 7 a 11
     }
